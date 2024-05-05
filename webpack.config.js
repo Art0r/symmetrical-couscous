66d5a3e90efe49d1fcbfdd6a2c84f80df8f91b91
Arthur Fernandes <arthur0139@gmail.com>
2024-05-05 14:33:39 -0300

    adding node.js for handling libs

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+
+module.exports = {
+    entry: { bulma: './assets/js/bulma.js' },
+    mode: 'production',
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'static/'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                },
+            },
+            {
+                test: /\.(scss|sass)$/,
+                use: [
+                    'style-loader',  // Inject styles into DOM
+                    'css-loader',    // Translates CSS into CommonJS
+                    'sass-loader',   // Compiles Sass to CSS
+                ],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif|ico)$/i,
+                use: {
+                    loader: 'file-loader',
+                    options: {
+                        name: '[name].[ext]',
+                        outputPath: 'images/',  // Specify the output path for images
+                    },
+                },
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
+};
